ef0af3e0ba81f579e2693bddc9a736f0828e1801
Osborn Nyberg palmèn <osborn.nyberg@gmail.com>
2022-06-08 18:14:47 +0200

    First commit

---
+++ views.py
@@ -0,0 +1,22 @@
+from flask import Blueprint, render_template, send_from_directory, request
+import os
+import json
+views = Blueprint("views", __name__, template_folder="templateFiles", static_folder="staticFiles")
+
+  
+@views.route("/")
+def home():
+    return render_template('home.html')
+
+@views.route("/stats")
+def stats():
+    return render_template('stats.html')
+
+@views.route("/favicon.ico")
+def favicon():
+    return send_from_directory(os.path.join(views.root_path, 'static'),'favicon.ico',mimetype='image/vnd.microsoft.icon')
+
+@views.route("datapost", methods=["POST"])
+def datapost():
+    data = request.json()
+    
